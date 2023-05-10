8f6f56f00aec2b948f86cf13e4c17e5b6fb43d0e
cj1607 <ucheokorojefferson@gmail.com>
2023-05-10 08:29:43 -0700

    alx solutions to 0x17 web stack debugging 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#use sed to fix typo
+exec { '/var/www/html/wp-settings.php':
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+}
